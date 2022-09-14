f0f918de3b6d32ace2946d67516ba2ad681b2332
montiho8 <montiho8@hawaii.edu>
2022-09-13 21:59:53 -1000

    Initial Commit

---
+++ HackwichTwo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackwichTwo
+//
+//  Created by Dominique Cortez-Montiho on 9/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackwichTwo/HackwichTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackwichTwoApp.swift
+//  HackwichTwo
+//
+//  Created by Dominique Cortez-Montiho on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct HackwichTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
